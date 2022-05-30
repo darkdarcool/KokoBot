a48037fb482b0c6a6896176d5d684236cdea35b2
Catermelon <66882633+darkdarcool@users.noreply.github.com>
2022-05-30 10:00:21 -0700

    Added dev code

--- src/main/java/me/darkdarcool/kokobot/Codes.java
+++ src/main/java/me/darkdarcool/kokobot/Codes.java
@@ -3,4 +3,5 @@ package me.darkdarcool.kokobot;
 public class Codes {
     public static int accountCreated = 100;
     public static int moneyAdded = 101;
+    public static int swearDeleted = 102;
 }
--- src/main/java/me/darkdarcool/kokobot/events/SwearWatcher.java
+++ src/main/java/me/darkdarcool/kokobot/events/SwearWatcher.java
@@ -1,8 +1,10 @@
 package me.darkdarcool.kokobot.events;
 
+import me.darkdarcool.kokobot.Codes;
 import me.darkdarcool.kokobot.DB;
 import me.darkdarcool.kokobot.User;
 import me.darkdarcool.kokobot.events.utils.Swears;
+import me.darkdarcool.kokobot.messages.Embed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.ArrayList;
@@ -15,7 +17,11 @@ public class SwearWatcher implements Event {
         if (badWords.size() > 0) {
             event.getMessage().delete().queue();
             // dm the user
-            event.getAuthor().openPrivateChannel().complete().sendMessage("You have had a message blocked for swearing.").queue();
+            event.getAuthor().openPrivateChannel().complete().sendMessageEmbeds(embed().build()).queue();
         }
+    }
+    private Embed embed() {
+        Embed embed = new Embed("Swear found", "You have had a message blocked for swearing.", "Developer code: " + Codes.swearDeleted);
+        return embed;
     }
 }
